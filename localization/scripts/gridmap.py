558f645398ee49dc9e00c7e7defec76f12f4bc5d
akanshumahajan <akanshumahajan@gmail.com>
2020-04-07 22:36:32 +0200

    fixed png creation

--- localization/scripts/gridmap.py
+++ localization/scripts/gridmap.py
@@ -227,18 +227,20 @@ class OccupancyGridMap:
 
 def create_grid(json_dict, grid_res=10):
     # grid_res is the number of rows/columns per meter => 10 means that each cell represents 1 dm x 1 dm
-    offset = json_dict['airspace']['min'][:2]       #[-10 , -10]
-    max_point = json_dict['airspace']['max'][:2]    # [10 , 10]
+    offset = json_dict['airspace']['min'][:2]
+    #print(offset)
+    max_point = json_dict['airspace']['max'][:2]
     
     x_len = max_point[0] - offset[0]
     y_len = max_point[1] - offset[1]
-    
-    grid = np.zeros((x_len*grid_res, y_len*grid_res), dtype=np.byte)
-    
+
+    grid = np.zeros((int(x_len*grid_res), int(y_len*grid_res)), dtype=np.byte)
+
     return grid, np.asarray(offset, dtype=np.float64)
 
 
-def draw_walls(json_dict, grid, offset, grid_res=10):
+
+def draw_walls(json_dict, grid, offset, grid_res=100):
     # line_res is how many points per meter there are to be between the start and the stop points of a wall
     line_res = 10 * grid_res
     for  d in json_dict['walls']:
@@ -246,12 +248,14 @@ def draw_walls(json_dict, grid, offset, grid_res=10):
         stop = np.array(d['plane']['stop'][:2]) - offset
         #print(start)
         #print(stop)
+
         n_points = int(np.linalg.norm(start - stop) * line_res)
         #print(n_points)
         for point in np.linspace(start, stop, n_points):
-            floored_point = np.floor(point * grid_res).astype(dtype=np.int)
+            floored_point = np.floor(point * (grid_res-1)).astype(dtype=np.int) # The floor of the scalar x is the largest integer i, such that i <= x. 
+            #print(floored_point)
             grid[floored_point[0]][floored_point[1]] = 1
-                
+            
     return grid
 
 # def grid_index_to_world_coord(self, index, offset, grid_res):
@@ -266,12 +270,12 @@ def draw_walls(json_dict, grid, offset, grid_res=10):
     
 #     return np.floor((coord - offset) * grid_res)
 
-def read_json(path = '/home/akanshu/dd2419_ws/src/course_packages/dd2419_resources/worlds_json/tutorial_1.world.json'):
+def read_json(path = '/home/akanshu/dd2419_ws/src/course_packages/dd2419_resources/worlds_json/milestone3.world.json'):
     with open(path) as f:
         json_dict = json.load(f)
 
-    grid, offset = create_grid(json_dict, grid_res=10)
-    grid = draw_walls(json_dict, grid, offset, grid_res=10)
+    grid, offset = create_grid(json_dict, grid_res=100)
+    grid = draw_walls(json_dict, grid, offset, grid_res=100)
     
     # Plot the grid as an image just to make sure it works
     img = np.zeros(tuple(list(grid.shape) + [3]))
